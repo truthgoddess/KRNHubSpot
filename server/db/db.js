9cb32af0e208712b6b7411264464900de26195fe
Kate Norton <katernorton@outlook.com>
2020-10-21 11:12:38 -0400

    got postgres back to connecting

--- server/db/db.js
+++ server/db/db.js
@@ -3,14 +3,13 @@ const pkg = require('../../package.json')
 
 const databaseName = pkg.name + (process.env.NODE_ENV === 'test' ? '-test' : '')
 
-console.log(databaseName)
-
 const db = new Sequelize(
   process.env.DATABASE_URL || `postgres://localhost:5432/${databaseName}`,
   {
     logging: false,
   }
 )
+
 module.exports = db
 
 // This is a global Mocha hook used for resource cleanup.
