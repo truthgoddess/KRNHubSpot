6ec020327ca6a19f059516f313103657bc5a5ca5
Kate Norton <katernorton@outlook.com>
2020-11-10 11:41:37 -0500

    added some styling

--- client/components/WindowContainers.js
+++ client/components/WindowContainers.js
@@ -3,13 +3,15 @@ import React, {useEffect} from 'react'
 import {MonacoEditor} from '../components'
 
 function WindowContainers(props) {
+  useEffect(() => {}, [])
+
   return (
     <div className="bottom-window-container">
-      <div id="left-container-main">
+      <div className="bottom-window-box" id="left-container-main">
         <MonacoEditor></MonacoEditor>
       </div>
-
-      <div className="program-window-container"></div>
+      <div className="handler"></div>
+      <div className="bottom-window-box" id="program-window-container"></div>
     </div>
   )
 }
